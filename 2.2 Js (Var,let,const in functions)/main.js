033160dd9f9ba41985748c5d2ca417d74a3e6db2
Shivang Gupta <86548591+sgdevelop@users.noreply.github.com>
2021-10-15 01:15:42 +0530

    Create main.js

---
+++ 2.2 Js (Var,let,const in functions)/main.js
@@ -0,0 +1,48 @@
+var i = 234;
+console.log(i);
+function ui(name)
+{
+    var i = 9;
+    // i = 9;
+    console.log(i);
+    return `This is a ${name} ui`;
+}
+console.log(ui("harry"),i);
+
+
+// let i = 234;
+// console.log(i);
+// function ui(name)
+// {
+//     let i = 9;
+//     // i = 9;
+//     console.log(i);
+//     return `This is a ${name} ui`;
+// }
+// console.log(ui("harry"),i);
+
+
+// const i = 234;
+// console.log(i);
+// function ui(name)
+// {
+//     const i = 9;
+//     // i = 9; //error
+//     console.log(i);
+//     return `This is a ${name} ui`;
+// }
+// console.log(ui("harry"),i);
+
+
+// if(1){
+//     var i = 234;
+//     // let i = 234; //error
+//     console.log(i);
+// }
+// console.log(i);
+// function ui(name) {
+//     let i = 9;
+//     console.log(i);
+//     return `This is a ${name} ui`;
+// }
+// console.log(ui("harry"), i);
