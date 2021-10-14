4c0b592f3e77968d84d3963035573d4d43c15cf1
Shivang Gupta <86548591+sgdevelop@users.noreply.github.com>
2021-10-14 22:59:19 +0530

    Create main.js

---
+++ 1.8 Js (switch case statement)/main.js
@@ -0,0 +1,20 @@
+//switch case statement
+// let age = 18;
+// let age = 28;
+let age = 38;
+// let age = 48;
+switch (age) {
+    case 18:
+        console.log("You are 18");
+        break;
+    case 28:
+        console.log("You are 28");
+        break;
+    case 38:
+        console.log("You are 38");
+        break;
+
+    default:
+        console.log("You are unknown age");
+        break;
+}
